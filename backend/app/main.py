979a8db14c9c07b09138c08f40ac06b0b8504dc7
Anik Chand <anikchand461@gmail.com>
2026-09-14 17:45:46 +0530

    allow production frontend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,6 +31,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
+        'https://torque-project.vercel.app',
     ],
     allow_credentials=True,
     allow_methods=["*"],
